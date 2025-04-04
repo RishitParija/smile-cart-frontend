45e40f9bb003f73ead00f49589b4fcb1c64562a5
Rishit Parija <85541344+RishitParija@users.noreply.github.com>
2025-04-04 12:57:19 +0530

    Added Carousel component

---
+++ src/components/Product.jsx
@@ -0,0 +1,14 @@
+import Carousel from "./Carousel";
+import { IMAGE_URLS } from "./constants";
+
+const Product = () => (
+  <div className="px-6 pb-6">
+    {/* rest of the code */}
+    <div className="w-2/5">
+      <Carousel imageUrls={IMAGE_URLS} title="Infinix Inbook" />
+    </div>
+    {/* rest of the code */}
+  </div>
+);
+
+export default Product;
