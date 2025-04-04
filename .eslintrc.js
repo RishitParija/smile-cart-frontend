c4d3f8d2ab04089f912e65501d1fc791f558d287
Rishit Parija <85541344+RishitParija@users.noreply.github.com>
2025-04-04 11:55:13 +0530

    lint error resolved

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = mergeDeepLeft(
     rules: {
       "react/react-in-jsx-scope": "off",
       "no-console": "off",
+      "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     },
   },
   defaultConfig
