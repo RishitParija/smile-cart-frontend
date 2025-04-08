777aa29ac146b5f1d4e3f0b82bbe323689e607a8
Rishit Parija <85541344+RishitParija@users.noreply.github.com>
2025-04-08 18:25:28 +0530

    Added cart page

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -3,9 +3,11 @@ import { useState, useEffect } from "react";
 import productsApi from "apis/products";
 import { Header, PageNotFound, PageLoader } from "components/commons";
 import AddToCart from "components/commons/AddToCart";
-import { Typography } from "neetoui";
+import useSelectedQuantity from "components/hooks/useSelectedQuantity";
+import { Typography, Button } from "neetoui";
 import { isNotNil, append } from "ramda";
 import { useParams } from "react-router-dom";
+import routes from "routes";
 
 import Carousel from "./Carousel";
 
@@ -14,6 +16,7 @@ const Product = () => {
   const { slug } = useParams();
   const [product, setProduct] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const { selectedQuantity, setSelectedQuantity } = useSelectedQuantity(slug);
   const fetchProduct = async () => {
     try {
       const response = await productsApi.show(slug);
@@ -70,7 +73,16 @@ const Product = () => {
           <Typography className="font-semibold text-green-600">
             {discountPercentage}% off
           </Typography>
-          <AddToCart {...{ availableQuantity, slug }} />
+          <div className="flex space-x-10">
+            <AddToCart {...{ availableQuantity, slug }} />
+            <Button
+              className="bg-neutral-800 hover:bg-neutral-950"
+              label="Buy now"
+              size="large"
+              to={routes.checkout}
+              onClick={() => setSelectedQuantity(selectedQuantity || 1)}
+            />
+          </div>
         </div>
       </div>
     </>
